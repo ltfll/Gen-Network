cd59a4ce106d3ef5405de35a951622bcf2ed4034
ltfll <74352844+ltfll@users.noreply.github.com>
2021-03-29 23:45:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ let sketch = function (p) {
   ];
   let minOctave = 3;
   let maxOctave = 5;
-  let velocity = 0.05;
+  let velocity = 0.4;
   let minInterval;
   let maxInterval;
   let uniformSound = true
