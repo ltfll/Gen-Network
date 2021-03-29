eeee30ecc2327ceeb30d82ea6d9ce2be9149f6e6
ltfll <74352844+ltfll@users.noreply.github.com>
2021-03-30 00:51:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -49,8 +49,8 @@ let sketch = function (p) {
   let MIN_INTERVAL = 1000;
   let MAX_INTERVAL = 2000;
   
-  Compressor = new p5.Compressor()
-  set(.003, 30, 12, -24, .25)
+  Compressor = new p5.Compressor();
+  set(.003, 30, 12, -24, .25);
 
 
   // Images
